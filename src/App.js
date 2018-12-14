f2e1a6316130194c5548bc7006e211b02faecf10
Ricardo Borges <ricardo93borges@gmail.com>
2018-12-13 23:23:31 -0200

    Saving in local storage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
-import './App.css';
+import marked from 'marked'
+import hljs from 'highlight.js'
 import MarkdownEditor from './components/MarkdownEditor'
+
+import './App.css';
+import 'highlight.js/styles/dracula.css'
+
+marked.setOptions({
+  highlight: (code) => {
+    return hljs.highlightAuto(code).value
+  }
+})
 
 class App extends Component {
   constructor(){
     super()
 
     this.state = {
-      value:''
+      value:'',
+      saving:false
     }
   }
 
+  componentDidMount(){
+    const value = localStorage.getItem('md')
+    this.setState({value})
+  }
+
+  componentDidUpdate(){
+    clearInterval(this.timer)
+    this.timer = setTimeout(() => {
+      this.handleSave(this.state.value)
+    }, 1000)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer)
+  }
+
   handleChange = (e) => {
-    this.setState({value:e.target.value})
+    this.setState({value:e.target.value, saving:true})
+  }
+
+  handleSave = (value) => {
+    localStorage.setItem('md',value)
+    this.setState({saving:false})
   }
 
   getMarkup = () => {
-    return {__html:this.state.value}
+    return {__html:marked(this.state.value)}
   }
 
   render() {
     return (
       <MarkdownEditor 
-        handleChange={this.handleChange} 
+        handleChange={this.handleChange}
+        saving={this.state.saving}
         value={this.state.value} 
         getMarkup={this.getMarkup}
       />
--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,12 +1,19 @@
-'use strict'
-
 import React from 'react'
+import PropTypes from 'prop-types';
 
-const MarkdownEditor = ({value, handleChange, getMarkup}) => (
+const MarkdownEditor = ({value, handleChange, saving, getMarkup}) => (
     <div className="App">
+        <p>{ saving ? 'Salvando ...' : 'Salvo!'}</p>
         <textarea autoFocus onChange={ handleChange } value={value} />
         <div className='view' dangerouslySetInnerHTML={getMarkup( )} />
     </div>
 )
+
+MarkdownEditor.propTypes = {
+    value: PropTypes.string.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    saving: PropTypes.bool.isRequired,
+    getMarkup: PropTypes.func.isRequired,
+}
 
 export default MarkdownEditor
